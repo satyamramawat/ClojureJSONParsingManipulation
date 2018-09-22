98d277be09a265ee5852fd1ece00886e69b473c8
Satyam Ramawat <satyam.ramawat@cerner.com>
2018-09-23 01:58:09 +0530

    Destructing with JSON parsing and storing values into DB

---
+++ Destructuring JSON Database/JSON_DB.clj
@@ -0,0 +1,35 @@
+(ns clojureexercise.JSON_DB
+(:require [clojure.java.jdbc :as sql]))  
+(require '[clojure.data.json :as json])
+(use 'clojure.java.io)
+
+;;Reading JSON file into Global Variable
+(def satyam-ramawat(json/read-str(slurp "c:/users/sr062324/desktop/satyam.json"):key-fn keyword))
+
+;; Main Function
+(defn person-info
+  [{:keys [f-name l-name phone company title height weight street city state zip]}]
+  
+  ;DB Connection
+  (def db{   
+         :classname "com.mysql.jdbc.Driver" 
+         :subprotocol "mysql"
+         :subname "//127.0.0.1:3306/testdb"
+         :user "root"
+         :password "password"})
+  
+  ;Calculating BMI
+  (def bmi (/ weight (* height height)))
+  
+  ;Inserting JSON Data into Database
+  (sql/insert! db :personinfo
+  {:firstname f-name :lastname l-name :phone phone 
+   :street street :city city :state state :zip zip 
+   :company company :title title :height height :weight weight :bmi bmi })
+  (println "Data Inserted")
+
+  ;View the content of table
+  (println(sql/query db["SELECT * from personinfo"])))
+  
+
+(person-info satyam-ramawat) ;; Passed the JSON data as Argument 
